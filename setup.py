60e2facedeb1413e0f3004d034a024850df8ab4e
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2018-11-12 01:00:09 +0530

    update metadata

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='torchtest',
+    version='0.3',
+    packages=['torchtest'],
+    license='GNU Affero General Public License v3 or later (AGPLv3+)',
+    long_description=open('README.adoc').read(),
+    install_requires=['torch'],
+)
---
+++ torchtest/__init__.py
@@ -0,0 +1,1 @@
+from torchtest.torchtest import *
